ae09d81b258e0f7d60503c2565b0067f0d07f420
Pal Jain <paljaiin@gmail.com>
2025-09-30 18:22:15 +0530

    Create bubble sort.cpp

---
+++ bubble sort.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int arr[], int size) {
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
+int main() {
+    int size;
+    cout << "Enter the number of elements in the array: ";
+    cin >> size;
+    int arr[size];
+    cout << "Enter " << size << " elements: ";
+    for (int i = 0; i < size; i++) {
+        cin >> arr[i];
+    }
+
+    bubbleSort(arr, size);
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
+/*
+Output:
+Enter the number of elements in the array: 5
+Enter 5 elements: 13 14 15 16 17
+Sorted array: 13 14 15 16 17
+*/
